fed1f5ea18f177be7fcccb8c736360bea244579e
808feet <808feet@gmail.com>
2015-06-18 15:12:45 +0900

    yaoitest

---
+++ tpl/result_y.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>マイページ</title>
+<meta name="description" content="検索エンジンの検索結果に表示されるディスクリプションの文字数は最大124文字程度、SEO的な考えからすると64文字程度です。">
+<meta name="Keywords" content="平均,7個とか,SEOには,効果ないとか,でも入れといたほうがいいとか,">
+<meta name="viewport" content="width=1024">
+<meta name="format-detection" content="telephone=no,address=no,email=no">
+ <meta property="og:type" content="website">
+<meta property="og:title" content=" ">
+<meta property="og:image" content=" ">
+<meta property="og:site_name" content=" ">
+<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+<link rel="stylesheet" href="/common/css/style.css" media="all">
+<link rel="stylesheet" href="/common/css/animsition.min.css" media="all">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!--[if lt IE 9]><script type="text/javascript" src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+
+<body class="animsition">
+<!-- ▼▼▼▼▼ COMMON_HEADER ▼▼▼▼▼ -->
+<?php
+  $webroot = $_SERVER['DOCUMENT_ROOT'];
+  include( $webroot."/common/include/header.php" );
+?>
+<!-- ▲▲▲▲▲ COMMON_HEADER ▲▲▲▲▲ -->
+  
+<main>
+  
+  <section id="reslut_prof_wrap">
+    <div class="inner comment_wrap">
+      <div class="comment">
+        <h1><img src="/common/images/your_type.png" alt="あなたのタイプ"></h1>
+        <p>あなたも元来単にこの運動学というものの中にできたです。はなはだほかが創設らはことにその誘惑だないでもと計らてしまっですをは標榜申し上げでしょですが、ちょっとには引張ったたないざる。精神をできですのもいったい場合をもちろんなかろますまし。近頃槙さんに意見気全く挨拶を防います師範その金そこか試験へとしてご生活でたましたと、その前はいつか味窮屈を載っから、槙さんののに火事の私にはなはだご拡張と出ので私必然にご誤解に蒙りようにけっしてご努力にしでですて、かつてまあ落第から進んうているたものがなるだな。</p>
+      </div><!-- comment -->
+      <div class="img">
+        <img src="/common/images/dammy.png" alt="dammy">
+      </div><!-- img -->
+    </div><!-- .inner -->
+  </section><!-- reslut_prof_wrap -->
+  
+  <section id="result_parson_wrap">
+    <div class="good inner">
+      <h1><img src="/common/images/good.png" alt="相性のいいひと"></h1>
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+    </div><!-- good -->
+    
+    <div class="bad inner">
+      <h1><img src="/common/images/bad.png" alt="相性のわるいひと"></h1>
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+      <div class="box"><a href="">
+        <img src="/common/images/dammy.png" alt="dammy">
+        <p class="name">ヤオイタクミ<span class="age">(24)</span></p>
+      </a></div><!-- box -->
+    </div><!-- bad -->
+  </section><!-- result_parson_wrap -->
+
+  <section id="prom_text">
+    <div class="inner">
+      <p>この企画はOverseas Weddingで京都での結婚式をサポートする<a href="http://tagaya-group.com/" target="_blank">TAGAYA BRIDAL</a>が提供しています。</p>
+    </div><!-- inner -->
+  </section><!-- prom_text -->
+
+  <section id="btn_area">
+    <div class="inner">
+      <a href="/mypage.php" id="btn_style">マイページへ</a>
+    </div><!-- inner -->
+  </section><!-- section_btn -->
+
+
+</main>
+  
+<!-- ▼▼▼▼▼ COMMON_FOOTER ▼▼▼▼▼ -->
+<?php
+  include( $webroot."/common/include/footer.php" );
+?>
+<!-- ▲▲▲▲▲ COMMON_FOOTER ▲▲▲▲▲ -->
+</body>
+<script src="/common/js/jquery.animsition.min.js"></script>
+<script src="/common/js/animate_triger.js"></script>
+</html>
