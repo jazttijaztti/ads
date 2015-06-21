f258976b4586937e540b62c0db64fa67cc7e7864
jazttijaztti <jazttijaztti@yahoo.co.jp>
2015-06-21 14:13:34 +0900

    test

---
+++ study_yamamoto.php
@@ -0,0 +1,15 @@
+<?php
+
+$get_it = "tagayaaaaa";
+$area = $array ('1' => '2' , 'testtest' = $get_it);
+$t[] = 'oo';
+$t[] = $area;
+$yaoi['a'] = $t;
+$yaoi['b'] = $area;
+$lin[] = $area;
+$lin[] = $yahoi;
+
+
+print "<pre>";
+var_dump($lin);
+
