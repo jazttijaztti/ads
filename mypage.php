439bc76e7027ff14ebb7c28b5f7d5da69cae29a6
jazttijaztti <jazttijaztti@yahoo.co.jp>
2015-06-22 20:58:29 +0900

    mypage.php

--- mypage.php
+++ mypage.php
@@ -1,35 +1,36 @@
 <?php
 require_once('fb/facebook.php');
 //use Facebook\FacebookRequest;
-//require_once('class/tool/login.php');
+require_once('class/tool/login.php');
 
 $base_url = 'http://localhost';
 $statics_url = $base_url.'/statics/';
 require_once('libralies/smarty/Smarty.class.php');
 header("Content-Type: text/html; charset=UTF-8");
 if (!$_SESSION['fb_id']) {
-   echo "ログインしていないからその処理をかく";
+   echo "ログインしていないからその処理をかく"; //林さん
 } 
+
 
 $fb_id = $_SESSION['fb_id'];
 $user_name = $_SESSION['fb_name'];
 $param = array('fb_id' =>$fb_id , 'user_name' =>$user_name);
 
 //まずこのfb_idがDBにあるのか確認する
-//$login = new login;
-//$b = $login->is_user($fb_id);
-//if ($b == true) {
+$login = new login;
+$b = $login->is_user($fb_id);
+if ($b == true) {
    //あるならuserテーブルからそのユーザの情報をもってくる
    $user_info = $login->get_user_info($fb_id);
 } else {
+   //fb_idがDBにないなら新規ユーザだから登録する
    $res = $Login->insert_new_user($param);
    if ($res) {
+       //登録したらそのユーザの情報をすべてもってくる
        $user_info = $login->get_user_info($fb_id);
    } else {
        //redirectする ?error=3
    }
-   //ないなら新規ユーザーだからinsertして、
-  //insertしてからそのinsertしたテーブルから情報を持ってくる
 }
 
 
